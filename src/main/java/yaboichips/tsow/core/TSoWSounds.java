9595d19c64b2f37f2142b9a69ed2b0aaf9f39e97
stereo <stereotypicalgaming0@gmail.com>
2024-12-11 12:35:13 +1300

    i fixed ponds but they are two blocks lower than they are supposed to be

--- src/main/java/yaboichips/tsow/core/TSoWSounds.java
+++ src/main/java/yaboichips/tsow/core/TSoWSounds.java
@@ -10,8 +10,10 @@ import static yaboichips.tsow.TSoW.MODID;
 public class TSoWSounds {
 
     public static HashMap<SoundEvent, ResourceLocation> SOUNDS = new HashMap<>();
-    public static SoundEvent CLOCK_AMBIENT = register(ResourceLocation.fromNamespaceAndPath(MODID, "clock_ambient"));
-    public static SoundEvent CLOCK_DIE = register(ResourceLocation.fromNamespaceAndPath(MODID, "clock_die"));
+    public static final SoundEvent CLOCK_AMBIENT = register(ResourceLocation.fromNamespaceAndPath(MODID, "clock_ambient"));
+    public static final SoundEvent CLOCK_DIE = register(ResourceLocation.fromNamespaceAndPath(MODID, "clock_die"));
+    public static final SoundEvent WIND_LOOP = register(ResourceLocation.fromNamespaceAndPath(MODID, "world/wind_loop"));
+    public static final SoundEvent SWAMP_AMBIENT = register(ResourceLocation.fromNamespaceAndPath(MODID, "world/swamp_loop"));
 
     public static SoundEvent register(ResourceLocation id) {
         SoundEvent event = SoundEvent.createVariableRangeEvent(id);
